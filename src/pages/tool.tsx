e8df0cb2daa1ff29c04fea4cc9a4917eccb35981
leyla_qiao <leyla_qiao@163.com>
2024-09-30 09:15:14 +0800

    update tool path

--- src/pages/tool.tsx
+++ src/pages/tool.tsx
@@ -5,10 +5,10 @@ export default function Tool() {
   return (
     <Layout title="工具" description="工具">
       <div className="container">
-        <a href="./ai-generate-px-area.html">ai-generate-px-area.html</a><br />
-        <a href="./mp4转gif.html">mp4转gif.html</a><br />
-        <a href="./公证收费计算器.html">公证收费计算器.html</a><br />
-        <a href="./进制转换器.html">进制转换器.html</a>
+        <a href="/tool/ai-generate-px-area.html">ai-generate-px-area.html</a><br />
+        <a href="/tool/mp4转gif.html">mp4转gif.html</a><br />
+        <a href="/tool/公证收费计算器.html">公证收费计算器.html</a><br />
+        <a href="/tool/进制转换器.html">进制转换器.html</a>
       </div>
     </Layout>
   );
